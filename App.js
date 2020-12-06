208c4f94228076bd98ae24ca55b5f4e93e9c25a9
Laio Andrade <laioand98@gmail.com>
2020-12-05 21:06:26 -0300

    add Home screen call

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './src/screens/home/Home';
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F2F1F1',
     alignItems: 'center',
     justifyContent: 'center',
   },
