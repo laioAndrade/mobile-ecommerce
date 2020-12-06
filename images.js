2b10ac6af53945cdf9b6a82d1cd19586efdaacf9
Laio Andrade <laioand98@gmail.com>
2020-12-05 23:16:52 -0300

    add path images file

---
+++ images.js
@@ -0,0 +1,11 @@
+export const images = {
+    312:  require("./assets/images/super-mario-odyssey.png"),
+    201: require("./assets/images/call-of-duty-infinite-warfare.png"),
+    102: require("./assets/images/the-witcher-iii-wild-hunt.png"),
+    99: require("./assets/images/call-of-duty-wwii.png"),
+    12: require("./assets/images/mortal-kombat-xl.png"),
+    74: require("./assets/images/shards-of-darkness.png"),
+    31: require("./assets/images/terra-media-sombras-de-mordor.png"),
+    420: require("./assets/images/fifa-18.png"),
+    501: require("./assets/images/horizon-zero-dawn.png")  
+}
